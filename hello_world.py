ad19b632515c452bad2f70a54a6af9b0a0edeaf1
Philip Seger <seger.philip@gmail.com>
2015-07-11 13:14:31 -0700

    does not act as expected

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,14 @@
 from flask import Flask
+from flask import Markup
 app = Flask(__name__)
-app.debug = True
 
 @app.route("/")
 def hello():
-  return "Hello world!"
+  return "Hello world! Possible pages include: Markup('<a>http://localhost:5000/test</a>')"
 
 @app.route("/test")
 def test():
-  return "Wow, you've visited this page {} times!"
+  return "Wow, you've visited this page {} times!".format('TEST')
 
 if __name__ == '__main__':
-  app.run()
+  app.run(debug = True)
